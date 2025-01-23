938b130425198a4913210867cedab8c538f97a53
fazil108 <onlinksolutionsinfo@gmail.com>
2025-01-23 20:06:24 +0530

    portfolio update ui

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -9,6 +9,7 @@ import Footer from '../components/Footer'
 const page = () => {
   return (
     <>
+      <div className="top-0 z-[-2] h-screen w-full fixed bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       <section className='py-24'>
         <div className='main_container'>
           <div className='mb-10'>
